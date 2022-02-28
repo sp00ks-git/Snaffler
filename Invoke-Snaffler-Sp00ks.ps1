267c767c0ce20f311f93e9b4812973642cb57ca8
sp00ks <56547010+sp00ks-git@users.noreply.github.com>
2022-02-28 12:41:49 +0000

    Update Invoke-Snaffler-Sp00ks.ps1

--- Invoke-Snaffler-Sp00ks.ps1
+++ Invoke-Snaffler-Sp00ks.ps1
@@ -4,7 +4,7 @@ function Invoke-Snaffler
     [CmdletBinding()]
     Param (
         [String]
-        $Command = "-u -s -y -o C:\Scripts\gt\snaffler-output.txt"
+        $Command = "-u -s -y -o C:\temp\snaffler-output.txt"
 
     )
     
